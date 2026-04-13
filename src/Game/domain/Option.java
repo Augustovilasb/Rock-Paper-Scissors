b4b58685bccf9d55d768f049e7b063bb98e07551
Augusto <augustovilasb@hotmail.com>
2026-04-13 19:43:11 +0100

    First commit game Rock, Paper & Scissors!

--- src/Game/domain/Option.java
+++ src/Game/domain/Option.java
@@ -1,4 +1,25 @@
 package Game.domain;
 
 public enum Option {
+
+    ROCK {
+        @Override
+        public Option win() {
+            return SCISSORS;
+        }
+    },
+    PAPER {
+        @Override
+        public Option win() {
+            return ROCK;
+        }
+    },
+    SCISSORS {
+        @Override
+        public Option win() {
+            return PAPER;
+        }
+    };
+
+    public abstract Option win();
 }
--- src/Game/domain/Service.java
+++ src/Game/domain/Service.java
@@ -1,4 +1,10 @@
 package Game.domain;
 
 public class Service {
+
+    public void whoWin(Option player, Option computer) {
+        if (player == computer) {
+            System.out.println("EMPATE");
+        }
+    }
 }
